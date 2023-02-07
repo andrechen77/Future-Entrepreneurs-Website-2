109ef05c75717483b79f26fa94451d3a070efb78
andrechen77 <andrexchen@gmail.com>
2023-02-06 18:11:39 -0600

    about page

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,15 +1,20 @@
-import { ThemeProvider } from '@mui/material/';
-import theme from './../theme';
+import { Box, Paper, Stack } from '@mui/material/';
+import AboutSection from '../components/AboutSection';
 
 const AboutUs = () =>{
 	return (
-		<ThemeProvider theme={theme}>
-            <div>
-                <h1>
-                    About Us Page!!!
-                </h1>
-            </div>
-		</ThemeProvider>
+        <Box>
+            <Paper sx={{typography: "h1"}}>
+                MEET THE TEAM
+            </Paper>
+            <Box sx={{padding: 3, bgcolor: "secondary.light"}}>
+                <Stack direction="column" spacing={10}>
+                    <AboutSection/>
+                    <AboutSection/>
+                    <AboutSection/>
+                </Stack>
+            </Box>
+        </Box>
 	);
 }
 
