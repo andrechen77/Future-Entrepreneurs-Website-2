862af9cb6cdc33d6946c98973c73638c87d25ead
andrechen77 <andrexchen@gmail.com>
2023-02-06 19:59:09 -0600

    add image banner on about us

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -2,13 +2,32 @@ import { Box, Paper, Stack } from '@mui/material/';
 import AboutSection from '../components/AboutSection';
 
 const AboutUs = () =>{
+    const bgImage = {
+        backgroundImage: `url("/images/chicago_skyline.jpg")`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        height: 400,
+    };
+    const centeredText = {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    };
+    const bannerStyle = {
+        ...bgImage,
+        ...centeredText,
+        typography: "h1",
+        color: "common.white",
+        fontWeight: 600,
+    }
+
 	return (
         <Box>
-            <Paper sx={{typography: "h1"}}>
+            <Paper sx={bannerStyle}>
                 MEET THE TEAM
             </Paper>
             <Box sx={{padding: 3, bgcolor: "secondary.light"}}>
-                <Stack direction="column" spacing={10}>
+                <Stack direction="column" spacing={3}>
                     <AboutSection/>
                     <AboutSection/>
                     <AboutSection/>
