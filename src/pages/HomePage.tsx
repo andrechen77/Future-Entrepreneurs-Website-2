54d9ca0cd1b4880e8c415551c7ecc7df2b499207
Siddharth Saha <siddharthsaha@dhcp-10-105-236-66.wireless.northwestern.private>
2023-02-13 17:39:57 -0600

    base home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,60 +1,34 @@
 import { ThemeProvider } from '@mui/material/';
 import theme from './../theme';
-import Sky from './../images/chicago_skyline.jpg'
-import Mission from './../images/icon.png'
 import { Box, Paper, Stack } from '@mui/material/';
-import { fontSize } from '@mui/system';
+import Banner from '../components/Banner';
+import MultiList from '../components/MultiList'
 
-
-const HomePage = () =>{
+const HomePage = () => {
 	return (
-		<ThemeProvider theme={theme}>
-            <div style = {{backgroundImage: `url(${Sky})`,
-		width: 1895,
-		height: 800}}>
-                <h1 style = {{
-					textAlign: 'center',
-					fontSize: 100,
-					marginTop: 0,
-					marginBottom: 5
-				}}>
-                    Future Entrepreneurs
-                </h1>
-				<p style = {{
-					textAlign: 'center',
-					fontSize: 75,
-					marginTop: 0
-
-				}}> The Future of Business</p>
-
-				<p style = {{
-					textAlign: 'center'
-				}}> insert intro video here </p>
-            </div>
-
-			<Box style = {{backgroundImage: `url(${Mission})`,
-		width: 400,
-		height: 370,
-		marginLeft: 300,
-		marginTop: 200,
-		marginBottom: 200}}>
-			<div style = {{fontSize: 75,
-			textAlign: 'right',
-			marginLeft: 600,
-			fontWeight: 'bold'}}>
-				Mission
-				<p style = {{
-					fontSize: 50,
-					fontWeight: 'normal',
-					textAlign: 'center'
-				}}> stuff</p>
-			</div>
-
-			</Box>
-		</ThemeProvider>
+        <>
+            <Box>
+                <Banner imgsrc="/images/chicago_skyline.jpg" ht='600px'>Strive for the best.</Banner>
+            </Box>
+            <Box sx={{
+                width: '100%',
+                height: '300px',
+                backgroundColor: '#465093'}}>
+                <MultiList></MultiList>
+                <Box sx={{  position: 'absolute',
+                        left: '900px',
+                        top: '750px',
+                        padding: '10px',
+                        width: '200px',
+                        height: '200px',
+                        backgroundColor: 'primary.dark',
+                        backgroundImage: `url("${"/images/chicago_skyline.jpg"}")`
+                        }}></Box>
+            </Box>
+        </>
 	);
 }
 
 export default HomePage;
 
-
+//<MultiList></MultiList>
