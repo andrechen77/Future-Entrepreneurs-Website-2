2954e5a49308361456bc34d00fd2e0fdd4d9e3d8
andrechen77 <andrexchen@gmail.com>
2023-02-15 20:31:13 -0600

    improve picture sizing for small screens

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,7 +1,7 @@
 /** @jsxImportSource @emotion/react */
 
 import { Box, Paper, Grid, Typography } from "@mui/material";
-import { createTheme, ThemeProvider, styled, makeStyles } from '@mui/material/styles'
+import { createTheme, ThemeProvider, styled, makeStyles, useTheme } from '@mui/material/styles'
 import React from "react";
 import { Children, cloneElement }  from "react";
 import "./style.css";
@@ -23,9 +23,13 @@ interface AboutSection {
 }
 
 function AboutSection({name, children, left, profile}: AboutSection) {
+	const theme = useTheme();
+
 	return (
 		<Paper elevation={1} sx={{ p: 2 }}>
-			<img style={{ float: "right" }} src={profile}/>
+			<Box sx={{ [theme.breakpoints.up("sm")]: { float: "right", maxWidth: 400 }, maxWidth: "100%"}}>
+				<img css={{width: "100%"}} src={profile}/>
+			</Box>
 			<Typography variant="h3">{name}</Typography>
 			<Typography variant="body1">{children}</Typography>
 		</Paper>
