92d57962d2317dd3bd342632b3c1a6663394bca4
hibakhatib <hibakhatib2025@u.northwestern.edu>
2023-02-15 23:32:57 -0600

    added real text from the business proposal

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,7 @@
-import { ThemeProvider } from '@mui/material/';
+// import { ThemeProvider } from '@mui/material/';
 import theme from './../theme';
-import { Box, Paper, Stack, Container} from '@mui/material/';
+import { Box, Container} from '@mui/material/';
 import Banner from '../components/Banner';
-import MultiList from '../components/MultiList'
 
 const HomePage = () => {
 	return (
@@ -12,27 +11,90 @@ const HomePage = () => {
             </Box>
 
             <Container maxWidth = "sm">
-			<Box style = {{backgroundImage: `url("${"/images/icon.png"}")`,
-		width: 800,
-		height: 400,
-        marginTop: 50,
-        marginBottom: 20,
-        display: 'flex'}}>
-			<div style = {{fontSize: 75,
-			textAlign: 'right',
-			marginLeft: 1000,
-			fontWeight: 'bold'}}>
-				Mission
-				<p style = {{
-					fontSize: 30,
-					fontWeight: 'normal',
-					textAlign: 'center'
-				}}> Our mission </p>
-			</div>
-
+			<Box style = {{fontSize: 100,
+			textAlign: 'center',
+			fontWeight: 'bold',
+            marginTop: 120,
+            marginBottom: 70}}>
+			Our Mission 
 			</Box>
+            </Container>
 
-        <Box style = {{backgroundImage: `url("${"/images/icon.png"}")`,
+            <Container sx={{ maxWidth:'60%'  }} maxWidth={false} >
+            <p style = {{
+					fontSize: 60,
+					fontWeight: 'normal',
+					textAlign: 'center',
+				}}> Our mission is to create an educational environment that provides opportunities and empowers 
+                young adults to learn business skills through both hands-on experience and training lessons. 
+                We plan to provide youth with resources that will increase their chances of achieving success 
+                in their business.   
+                <br></br> 
+                <br></br> 
+                Our  vision  is  to  build  an  organization  that  has  the  right  programs,  information,  insights, 
+                experience, and services that help youth adults build successful and profitable businesses.  
+                <br></br> 
+                <br></br> 
+                We  understand  that  embarking  on  such  a  journey  takes  a  huge  amount  of  time,  effort,  and 
+                investment.  Knowing  where  and  how  to  begin  can  be  equally  daunting;  this  is  why  Future 
+                Entrepreneurs USA was created.
+                <br></br> 
+                <br></br> 
+                <br></br> 
+                <br></br> 
+                {/* <Box style = {{backgroundImage: "/images/homepage.png"}}>
+
+                "Future Entrepreneurs USA is a corporation that teaches hands-on skills to young adults looking to build 
+                their business in a cafe/smoothie shop. Aside from this, we also offer an investment club program, and an 
+                AV space/studio to create marketing pieces."
+                </Box> */}
+                {/* <img src = "/images/homepage.png" width = "100%"></img> */}
+                <br></br> 
+                <br></br> 
+
+
+                <iframe width="2240" height="1260" 
+                    src="https://www.youtube.com/embed/92ZmzD70sOU" 
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    > 
+                </iframe>
+
+                </p>
+
+                
+
+
+
+                {/* <Box sx={{ 
+                        position: 'relative',
+                        display: "inline-flex",
+                        justify: 'center',
+                        padding: '10px',
+                        width: "100%",
+                        height: 500,
+                        flex: 1
+                        }}>
+                </Box> */}
+
+                </Container>
+            
+        </>
+	);
+}
+
+export default HomePage;
+
+
+
+
+
+ 
+
+
+
+
+{/* <Box style = {{backgroundImage: `url("${"/images/icon.png"}")`,
 		width: 800,
 		height: 400}}>
 			<div style = {{fontSize: 75,
@@ -47,46 +109,4 @@ const HomePage = () => {
 				}}>  </p>
 			</div>
 
-			</Box>
-            </Container>
-            
-            {/* <Box>
-                <MultiList></MultiList>
-                <Box sx={{ 
-                        position: 'relative',
-                        left: '900px',
-                        display: "inline-flex",
-                        padding: '10px',
-                        width: '400px',
-                        height: '400px',
-                        backgroundColor: 'primary.dark',
-                        backgroundImage: `url("${"/images/chicago_skyline.jpg"}")`
-                        }}>
-
-                </Box>
-            </Box> */}
-        </>
-	);
-}
-
-export default HomePage;
-
-
-
-
-// Our mission is to create an educational environment that provides opportunities and empowers 
-// young adults to learn business skills through both hands-on experience and training lessons. 
-// We plan to provide youth with resources that will increase their chances of achieving success 
-// in their business.   
- 
-// Our  vision  is  to  build  an  organization  that  has  the  right  programs,  information,  insights, 
-// experience, and services that help youth adults build successful and profitable businesses.  
- 
-// We intend to promote mentorship, intelligence skill-building support, assistance with access to 
-// other professionals at the top corporate level and opportunities for our clients to launch their 
-// businesses. We are committed to helping the youths in our community thrive and become the 
-// best in their industry.  
- 
-// We  understand  that  embarking  on  such  a  journey  takes  a  huge  amount  of  time,  effort,  and 
-// investment.  Knowing  where  and  how  to  begin  can  be  equally  daunting;  this  is  why  Future 
-// Entrepreneurs USA was created. 
+			</Box> */}
