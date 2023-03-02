9b4b4b0ac3a6a4b29c1fa4a318664b36bbdff4cb
taeoldest <alextaeyounglee@gmail.com>
2023-03-01 20:15:41 -0600

    updated padding of get involved

--- src/pages/GetInvolved.tsx
+++ src/pages/GetInvolved.tsx
@@ -11,9 +11,9 @@ const Item = styled(Paper)(({ theme }) => ({
 
 const GetInvolved = () =>{
 	return (
-        <Box sx={{backgroundColor: 'secondary.main'}}>
+        <Box sx={{backgroundColor: 'secondary.main', padding:5}}>
             <div>
-            <Typography align="center" variant="h2">
+            <Typography align="center" variant="h2" >
                 Get Involved
             </Typography>
 
@@ -21,7 +21,7 @@ const GetInvolved = () =>{
                 Become a part of the team. Change a life
             </Typography>
 
-            <Typography align="center" variant="body1">
+            <Typography align="center" variant="body1" sx={{padding:3}}>
                 
                 Lorem ipsum dolor sit amet. consectetur adipiscing elit. 
                 Sed dictum orci ut augue vestibulum vestibulum sed bibendum quam.
@@ -31,27 +31,27 @@ const GetInvolved = () =>{
 
             <Container sx={{ flexGrow: 0.1}}>
                 <Grid container spacing={2} justifyContent = 'center'>
-                    <Grid item xs={5}>
+                    <Grid item xs={5} sx={{padding:2}}>
                         <Item>
                             <img src='/images/icon.png' alt="Logo" />
                         </Item>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={5} >
                         <Item>
                             <img src='/images/icon.png' alt="Logo" />
                         </Item>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={5} >
                         <Typography align="left" variant="h3">
                             Donate
                         </Typography>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={5} >
                         <Typography align="left" variant="h3">
                             Volunteer
                         </Typography>
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={5} >
                         <Typography align="left" variant="body1">
                             Lorem ipsum dolor sit amet. consectetur adipiscing elit. 
                             Sed dictum orci ut augue vestibulum vestibulum sed bibendum quam.
