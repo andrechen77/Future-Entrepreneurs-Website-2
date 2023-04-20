c9e1ee2201703055daaf30d1f7498938f5cb6fb5
taeoldest <alextaeyounglee@gmail.com>
2023-04-19 20:27:54 -0500

    centered the calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,15 +1,37 @@
-import { ThemeProvider } from '@mui/material/';
+import { ThemeProvider, Box, Typography } from '@mui/material/';
 import theme from './../theme';
+
 
 const Calendar = () =>{
 	return (
 		<ThemeProvider theme={theme}>
+<<<<<<< Updated upstream
             <div>
                 <h1>
                     Calendar Page!!!
                 </h1>
             </div>
 		</ThemeProvider>
+=======
+             <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height = {800}
+                
+                
+            >
+                
+                <iframe
+                    src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=UTC&src=ZnV0dXJlZW50cmVwcmVuZXVydXNhQGdtYWlsLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
+                    width={800}
+                    height={600}
+                    title="Calendar"
+                />
+                
+            </Box>
+    		</ThemeProvider>
+>>>>>>> Stashed changes
 	);
 }
 
