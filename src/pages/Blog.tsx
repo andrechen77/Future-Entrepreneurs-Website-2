c7d5e616febd624673d0369528effe2ce5fd3740
andrechen77 <andrexchen@gmail.com>
2023-04-19 19:45:06 -0500

    make blog retrieve and display all posts in json

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,15 +1,28 @@
-import { ThemeProvider } from '@mui/material/';
+import { Box, ThemeProvider } from '@mui/material/';
 import theme from './../theme';
+import { useEffect, useState } from 'react';
+
+const blogId = "2770090416290081688";
+const apiKey = "AIzaSyCddmIa64GdGVmtCWEjQT2RMzB6jExm30E";
+
 
 const Blog = () =>{
+    const [posts, setPosts] = useState({});
+
+    const getPosts = async () => {
+        const response = await fetch(`https://www.googleapis.com/blogger/v3/blogs/${blogId}/posts?key=${apiKey}`);
+        const json = await response.json();
+        setPosts(json);
+    }
+
+    useEffect(() => {
+        getPosts();
+    }, []);
+
 	return (
-		<ThemeProvider theme={theme}>
-            <div>
-                <h1>
-                    Blog!!!
-                </h1>
-            </div>
-		</ThemeProvider>
+		<Box>
+           {JSON.stringify(posts)}
+        </Box>
 	);
 }
 
