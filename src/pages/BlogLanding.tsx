d60cf616332fb25889f6e2879b9a6d0bb4e3b4d1
andrechen77 <andrexchen@gmail.com>
2023-04-27 16:51:49 -0500

    fix blog landing to use client side navigation

--- src/pages/BlogLanding.tsx
+++ src/pages/BlogLanding.tsx
@@ -2,39 +2,49 @@ import { Box, Container, ImageList, ImageListItem, ImageListItemBar, Stack } fro
 import Banner from '../components/Banner';
 import ImageSlider from '../components/ImageSlider';
 import { BlogPost } from '../apis/bloggerApi';
+import { useNavigate } from 'react-router-dom';
 
 export default function BlogLanding({ posts }: { posts: BlogPost[]}) {
 	const images = posts.map(post => ({ url: post.coverImage }));
+
+	const imageSlider = (
+		<ImageSlider
+			height={500}
+			images={images}
+			showBullets
+			showNavs
+		/>
+	);
+
+	const navigate = useNavigate();
+	const postGrid = (
+		<ImageList cols={3}>
+			{posts.map(post => (
+				<ImageListItem
+					key={post.id}
+					style={{ cursor: "pointer" }}
+					onClick={() => navigate(post.id)}
+				>
+					<img
+						src={post.coverImage}
+						alt={post.title}
+					/>
+					<ImageListItemBar
+						title={post.title}
+						subtitle={post.updated.toLocaleDateString()}
+					/>
+				</ImageListItem>
+			))}
+		</ImageList>
+	);
 
 	return (
 		<Box>
 			<Banner imgsrc="/images/chicago_skyline.jpg">FE BLOG</Banner>
 			<Container>
 				<Stack sx={{ p: 3 }}>
-					<ImageSlider
-						height={500}
-						images={images}
-						showBullets
-						showNavs
-					/>
-					<ImageList cols={3}>
-						{posts.map(post => (
-							<ImageListItem
-								key={post.id}
-								style={{ cursor: "pointer" }}
-								onClick={() => document.location.assign(`Blog/${post.id}`)}
-							>
-								<img
-									src={post.coverImage}
-									alt={post.title}
-								/>
-								<ImageListItemBar
-									title={post.title}
-									subtitle={post.updated.toLocaleDateString()}
-								/>
-							</ImageListItem>
-						))}
-					</ImageList>
+					{imageSlider}
+					{postGrid}
 				</Stack>
 			</Container>
 		</Box>
