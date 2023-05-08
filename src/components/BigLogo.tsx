b7aab16eb1c47b71557f7d7402796cbb13dbeadd
andrechen77 <andrexchen@gmail.com>
2023-05-08 00:13:10 -0500

    reconcile theme changes between master/footer

--- src/components/BigLogo.tsx
+++ src/components/BigLogo.tsx
@@ -1,19 +1,12 @@
-/** @jsxImportSource @emotion/react */
-
-const logoHeight = 80;
 const style = {
 	maxWidth: 300,
 	maxHeight: 200,
-	"&:hover": {
-		maxWidth: 301,
-		maxHeight: logoHeight + 1,
-	},
 };
 
 function BigLogo() {
 	return (
 		<a href="/" onClick={() => console.log("I've been clicked")}>
-			<img css={style} src={"images/logobetter.png"} alt="logo"/>
+			<img style={style} src={"images/logobetter.png"} alt="logo"/>
 		</a>
 	);
 }
--- src/theme.tsx
+++ src/theme.tsx
@@ -30,6 +30,10 @@ const theme = createTheme({
 			fontFamily: 'Commissioner',
 			fontSize: 24,
 		},
+		body2 :{
+			fontFamily: 'Commissioner',
+			fontSize: 18,
+		},
 	},
 	palette: {
 		primary: {
