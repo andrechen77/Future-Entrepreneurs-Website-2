a4ee282bfe54fe25104879053e29be3f29cfc819
andrechen77 <andrexchen@gmail.com>
2023-05-10 20:52:14 -0500

    add rudimentary contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,15 +1,52 @@
-import { ThemeProvider } from '@mui/material/';
-import theme from './../theme';
+import { Button, Box, Container, Grid, TextField, Typography, Stack } from "@mui/material";
+import Banner from "../components/Banner";
+import { useState } from "react";
 
-const Contact = () =>{
+const Contact = () => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [message, setMessage] = useState("");
+
+	const handleSubmission = () => {
+		console.log(`${name} sent a message with email ${email}:\n${message}`)
+		// TODO
+	};
+
+	const emailForm = (
+		<Box>
+			<Typography variant="h3" sx={{ my: 1 }}>Send us an email</Typography>
+			<Stack spacing={1} alignItems="center">
+				<TextField label="Name" fullWidth value={name} onChange={e => setName(e.target.value)}/>
+				<TextField label="Email Address" fullWidth value={email} onChange={e => setEmail(e.target.value)}/>
+				<TextField label="Message" fullWidth multiline rows={7} value={message} onChange={e => setMessage(e.target.value)}/>
+				<Button variant="contained" onClick={handleSubmission}>Submit</Button>
+			</Stack>
+		</Box>
+	);
+
+	const sidebar = (
+		<Box sx={{ typography: "body1", width: 300 }}>
+			<Typography variant="h3">Info</Typography>
+			<Box>(insert address here)</Box>
+			<Box>(insert email here)</Box>
+			<Box>(insert phone number here)</Box>
+		</Box>
+	);
+
 	return (
-		<ThemeProvider theme={theme}>
-            <div>
-                <h1>
-                    Contact Page!!!
-                </h1>
-            </div>
-		</ThemeProvider>
+		<Box>
+			<Banner imgsrc="/images/chicago_skyline.jpg" ht={200}>CONTACT US</Banner>
+			<Container>
+				<Grid container spacing={3} sx={{ p: 3 }}>
+					<Grid item xs={12} md>
+						{emailForm}
+					</Grid>
+					<Grid item xs={12} md="auto">
+						{sidebar}
+					</Grid>
+				</Grid>
+			</Container>
+		</Box>
 	);
 }
 
